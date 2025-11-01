ec6d6c8103f3403411f4ed0100d80ff6dda2ab00
cigkaisme <huylvmps40034@gmail.com>
2025-11-02 04:38:00 +0700

    Cải thiện phần Header, slider
    Chưa hoàn thành lắm về Header, footer,...

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/layout/header/header.component';
+import { HeaderComponent } from './components/layout/header-home/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomeComponent } from './pages/Home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class App {
   protected title = 'client';
--- client/src/app/components/layout/header-home/header.component.ts
+++ client/src/app/components/layout/header-home/header.component.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
